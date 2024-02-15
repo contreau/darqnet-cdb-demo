648882d2350e4a1df33aac5401bc17709c43818c
zenDev-2 <72581161+contreau@users.noreply.github.com>
2024-02-15 00:43:34 -0500

    fixed signature hashing

--- src/lib/WalletLogin.js
+++ src/lib/WalletLogin.js
@@ -4,7 +4,8 @@ import { mainnet, arbitrum } from "viem/chains";
 import { DID } from "dids";
 import { Ed25519Provider } from "key-did-provider-ed25519";
 import { getResolver } from "key-did-resolver";
-import { SHA256 } from "@stablelib/sha256";
+import * as u8a from "uint8arrays";
+import { hash } from "@stablelib/sha256";
 
 const projectId = "c6c57240c4eee81a0f1fc1b23c905bea";
 
@@ -110,99 +111,98 @@ export class WalletLogin extends HTMLElement {
     `;
     const shadow = this.attachShadow({ mode: "open" });
     shadow.append(wc.content.cloneNode(true));
+    this.finished = new Promise((resolve) => {
+      async function executeSignature() {
+        // change text in button
+        signButton.innerText = "Check your mobile wallet";
+        try {
+          const signature = await signMessage({
+            message: "I bless this offering",
+            method: "personal_sign",
+          });
+          const seed = hash(u8a.fromString(signature.slice(2), "base16"));
+          const did = await authenticateDID(seed);
+          console.log(did.id);
 
-    const signButton = shadow.querySelector(".sign-button");
-    // watches web3 modal for changes
-    watchAccount((account) => {
-      console.log("account changes:", account);
-      if (account.isConnected) {
-        currentlySignedIn = true;
-        getWalletInfo(account.address);
+          // const ceramic = new CeramicClient("https://ceramic-clay.3boxlabs.com");
+          // ceramic.did = did;
+          // const doc = await TileDocument.create(
+          //   ceramic,
+          //   null,
+          //   { deterministic: true },
+          //   { anchor: false, publish: false }
+          // );
+
+          // const cp = "What will you conjure by the summer solstice?";
+          // const ep = "Feel into the moment and capture its essence!";
+          // const dp = "What is your biggest dream for the new year?";
+          // const c = ["Conjuration sample.."];
+          // const e = ["Essence sample.."];
+          // const d = ["Dream sample.."];
+
+          // const jwe = await did.createDagJWE(
+          //   {
+          //     cp,
+          //     ep,
+          //     dp,
+          //     c,
+          //     e,
+          //     d,
+          //   },
+          //   [did.id]
+          // );
+          // console.log(JSON.stringify(jwe));
+          // await doc.update(jwe);
+
+          await disconnectAccount();
+          resolve(true);
+        } catch (err) {
+          console.error(err);
+          signButton.innerText = "Signature rejected.";
+          await new Promise((resolve) => {
+            setTimeout(() => {
+              resolve(null);
+            }, 2000);
+          });
+          signButton.innerText = "I bless this offering";
+        }
       }
-      if (account.isDisconnected) {
-        currentlySignedIn = false;
+
+      const signButton = shadow.querySelector(".sign-button");
+      // watches web3 modal for changes
+      watchAccount((account) => {
+        // console.log("account changes:", account);
+        if (account.isConnected) {
+          currentlySignedIn = true;
+          getWalletInfo(account.address);
+        }
+        if (account.isDisconnected) {
+          currentlySignedIn = false;
+        }
+      });
+
+      // Retrieves wallet address and creates signature button
+      async function getWalletInfo(accountAddress) {
+        const account = await new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(accountAddress);
+          }, 10);
+        });
+        signButton.style.opacity = "1";
+        signButton.addEventListener("click", () => executeSignature(account));
+      }
+
+      // Create DID from hash
+      async function authenticateDID(seed) {
+        const provider = new Ed25519Provider(seed);
+        const did = new DID({ provider, resolver: getResolver() });
+        await did.authenticate();
+        return did;
+      }
+      // Disconnects wallet account - wagmi method doesn't work by itself in the try/catch block
+      async function disconnectAccount() {
+        await disconnect();
       }
     });
-
-    // Retrieves wallet address and creates signature button
-    async function getWalletInfo(accountAddress) {
-      const account = await new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(accountAddress);
-        }, 10);
-      });
-      signButton.style.opacity = "1";
-      signButton.addEventListener("click", () => executeSignature(account));
-    }
-
-    // Create DID from hash
-    async function authenticateDID(seed) {
-      const provider = new Ed25519Provider(seed);
-      const did = new DID({ provider, resolver: getResolver() });
-      await did.authenticate();
-      return did;
-    }
-    // Disconnects wallet account - wagmi method doesn't work by itself in the try/catch block
-    async function disconnectAccount() {
-      await disconnect();
-    }
-
-    async function executeSignature() {
-      // change text in button
-      signButton.innerText = "Check your mobile wallet";
-      try {
-        const signature = await signMessage({
-          message: "I bless this offering",
-          method: "personal_sign",
-        });
-        const hashFn = new SHA256();
-        const seed = hashFn.digest(signature);
-        const did = await authenticateDID(seed);
-        console.log(did.id);
-
-        // const ceramic = new CeramicClient("https://ceramic-clay.3boxlabs.com");
-        // ceramic.did = did;
-        // const doc = await TileDocument.create(
-        //   ceramic,
-        //   null,
-        //   { deterministic: true },
-        //   { anchor: false, publish: false }
-        // );
-
-        // const cp = "What will you conjure by the summer solstice?";
-        // const ep = "Feel into the moment and capture its essence!";
-        // const dp = "What is your biggest dream for the new year?";
-        // const c = ["Conjuration sample.."];
-        // const e = ["Essence sample.."];
-        // const d = ["Dream sample.."];
-
-        // const jwe = await did.createDagJWE(
-        //   {
-        //     cp,
-        //     ep,
-        //     dp,
-        //     c,
-        //     e,
-        //     d,
-        //   },
-        //   [did.id]
-        // );
-        // console.log(JSON.stringify(jwe));
-        // await doc.update(jwe);
-
-        await disconnectAccount();
-      } catch (err) {
-        console.error(err);
-        signButton.innerText = "Signature rejected.";
-        await new Promise((resolve) => {
-          setTimeout(() => {
-            resolve(null);
-          }, 2000);
-        });
-        signButton.innerText = "I bless this offering";
-      }
-    }
-
-    this.finished = new Promise((resolve) => {});
   }
 }
