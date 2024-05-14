3b72eeb7acb9b2b2b76461e62ac52b8baa0ee529
zenDev-2 <72581161+contreau@users.noreply.github.com>
2024-05-14 18:55:27 -0400

    added deploy script

--- scripts/comp.sh
+++ scripts/comp.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 composedb composite:create schemas/Shard.graphql --output composites/shard.json --did-private-key $(cat priv.key) &&
 composedb composite:merge composites/ritual.json composites/shard.json --output composites/demo-composites-merged.json &&
+composedb composite:deploy composites/demo-composites-merged.json --did-private-key $(cat priv.key) &&
 composedb composite:compile composites/demo-composites-merged.json composites/demo-runtime.json &&
 composedb graphql:server composites/demo-runtime.json --did-private-key $(cat priv.key) --port=35000 --graphiql
