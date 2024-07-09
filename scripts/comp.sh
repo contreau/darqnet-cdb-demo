37cd567a993f263de8e6503b52f735da0f121281
zenDev-2 <72581161+contreau@users.noreply.github.com>
2024-07-09 00:45:24 -0400

    removed graphiql line from script (not necessary)

--- scripts/comp.sh
+++ scripts/comp.sh
@@ -2,5 +2,4 @@
 composedb composite:create schemas/Shard.graphql --output composites/shard.json --did-private-key $(cat priv.key) &&
 composedb composite:merge composites/ritual.json composites/shard.json --output composites/demo-composites-merged.json &&
 composedb composite:deploy composites/demo-composites-merged.json --did-private-key $(cat priv.key) &&
-composedb composite:compile composites/demo-composites-merged.json composites/demo-runtime.json &&
-composedb graphql:server composites/demo-runtime.json --did-private-key $(cat priv.key) --port=35000 --graphiql
+composedb composite:compile composites/demo-composites-merged.json composites/demo-runtime.json
